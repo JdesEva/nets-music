6bd172e89954fcfa8606a5f98df2aad105cb4bd2
Jdeshz <17858801330@163.com>
2019-03-19 17:32:08 +0800

    文件名修改

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './model/header'
 import HeaderWrapper from './model/headerWrapper'
-import Main from './model/content/main'
+import Main from './model/views/main'
 
 class App extends Component {
   render() {
